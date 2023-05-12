d9e6f292d44e896346aa64536e769289f330d01a
Mateusz Kuriata <mateusz.kuriata@amsterdamstandard.com>
2023-05-12 11:10:54 +0200

    changed tldraw to excalidraw

--- src/pages/board/board.tsx
+++ src/pages/board/board.tsx
@@ -1,70 +1,55 @@
-import {
-  App,
-  Canvas,
-  ContextMenu,
-  getUserData,
-  TldrawEditor,
-  TldrawUi,
-  TldrawUiContextProvider,
-  TLInstanceId,
-  useApp,
-} from '@tldraw/tldraw'
+import { TLInstanceId } from '@tldraw/tldraw'
 import '@tldraw/tldraw/editor.css'
 import '@tldraw/tldraw/ui.css'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import useFirestore from '../../hooks/useFirestore'
-import { Box } from '@mui/material'
+import { Box, LinearProgress, Stack, Typography } from '@mui/material'
 import { useParams } from 'react-router-dom'
+import { Excalidraw } from '@excalidraw/excalidraw'
+import { ExcalidrawInitialDataState } from '@excalidraw/excalidraw/types/types'
+import { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types'
+import { throttle } from 'lodash'
 
-export default function Board() {
+export function Board({ elements }: ExcalidrawInitialDataState) {
   const instanceId = useParams()?.boardId as TLInstanceId
-  const userData = getUserData()
-  const { getSingleCollectionItem } = useFirestore()
+  const { addToDoc } = useFirestore()
 
-  const onMount = useCallback((app: App) => {
-    getSingleCollectionItem({ collectionId: 'boardsContent', id: instanceId }).then((data) => {
-      app.store.mergeRemoteChanges(() => {
-        app.store.put(data?.records || [])
+  const onChange = (elements: readonly ExcalidrawElement[]) => {
+    throttle(() => {
+      addToDoc({
+        collectionId: 'boardsContent',
+        data: { elements },
+        id: instanceId,
       })
-    })
-  }, [])
+    }, 100)()
+  }
 
   return (
     <Box width={'100%'} height={'100%'}>
-      <TldrawEditor instanceId={instanceId} onMount={onMount} userId={userData.id}>
-        <TldrawUiContextProvider>
-          <ContextMenu>
-            <Canvas />
-            <InsideOfAppContext />
-          </ContextMenu>
-          <TldrawUi />
-        </TldrawUiContextProvider>
-      </TldrawEditor>
+      <Excalidraw initialData={{ elements }} onChange={(elements) => onChange(elements)} />
     </Box>
   )
 }
 
-const InsideOfAppContext = () => {
-  const app = useApp()
-  const { addToDoc } = useFirestore()
-
-  let timerId: string | number | NodeJS.Timeout | undefined
-  function debounce(callback: () => void, delay: number | undefined) {
-    clearTimeout(timerId)
-    timerId = setTimeout(callback, delay)
-  }
+const LoadBoard = () => {
+  const instanceId = useParams()?.boardId as TLInstanceId
+  const [elements, setElements] = useState<ExcalidrawInitialDataState['elements'] | null>(null)
+  const { getSingleCollectionItem } = useFirestore()
 
   useEffect(() => {
-    app.store.listen(() => {
-      debounce(() => {
-        addToDoc({
-          collectionId: 'boardsContent',
-          data: { records: app.store.allRecords() },
-          id: app.instanceId,
-        })
-      }, 100)
+    getSingleCollectionItem({ collectionId: 'boardsContent', id: instanceId }).then((data) => {
+      setElements(data?.elements)
     })
-  })
+  }, [])
 
-  return null
+  return elements ? (
+    <Board elements={elements} />
+  ) : (
+    <Stack m={10} spacing={2}>
+      <Typography textAlign={'center'}>Loading...</Typography>
+      <LinearProgress />
+    </Stack>
+  )
 }
+
+export default LoadBoard
