f5a2e56275af91915021d8fe6957dafa187ee897
Mateusz Kuriata <mateusz.kuriata@mkuriata.com>
2023-06-01 11:51:45 +0200

    feat(DAR-23): fix refetch list of board after add or remove

--- src/pages/boards/boardsPage.tsx
+++ src/pages/boards/boardsPage.tsx
@@ -1,12 +1,13 @@
 import { Stack } from '@mui/material'
 import { collection, query, where, getDocs, doc, onSnapshot } from 'firebase/firestore'
-import { SetStateAction, useContext, useEffect, useState } from 'react'
-import { FirebaseUserContext, FirebaseUserContextType } from '../../providers/firebaseUserProvider'
+import { SetStateAction, useEffect, useState } from 'react'
+import { useUserContext } from '../../providers/firebaseUserProvider'
 import BoardTable, { BoardEntity } from './components/boardTable/boardTable'
 import { db } from '../../config/firebase'
 
 const BoardsPage = () => {
-  const { user } = useContext(FirebaseUserContext) as FirebaseUserContextType
+  const { user } = useUserContext()
+
   const [boards, setBoards] = useState<BoardEntity[]>([])
 
   useEffect(() => {
@@ -14,17 +15,18 @@ const BoardsPage = () => {
       onSnapshot(doc(db, 'users', user?.id), (doc) => {
         const userData = doc.data()
 
-        if (userData && userData.userBoards.length) {
+        if (!userData) return
+
+        if (userData.userBoards.length) {
           const q = query(collection(db, 'boards'), where('boardId', 'in', userData.userBoards))
           getDocs(q).then((data) => {
             const newBoards: SetStateAction<BoardEntity[]> = []
 
-            data.forEach((doc) => {
-              const board = doc.data()
-              newBoards.push(board as BoardEntity)
-            })
+            data.forEach((doc) => newBoards.push(doc.data() as BoardEntity))
             setBoards(newBoards)
           })
+        } else {
+          setBoards([])
         }
       })
   }, [])
