72ea86c09715917e40878b8faf3e42f097227085
Mateusz Kuriata <mateusz.kuriata@mkuriata.com>
2023-06-15 11:29:34 +0200

    feat(DAR-37): added helper for inject before children with portal

--- src/pages/board/components/excalidrawBoard/additionalButtons.tsx
+++ src/pages/board/components/excalidrawBoard/additionalButtons.tsx
@@ -2,7 +2,7 @@ import ViewModeToggle from '../viewModeToggle/viewModeToggle'
 import ShareButton from '../shareButton/shareButton'
 import { Stack } from '@mui/material'
 import { useDevice } from '@excalidraw/excalidraw'
-import { createPortal } from 'react-dom'
+import InjectBefore from '../../../../shared/helpers/injectBefore'
 
 type Props = {
   instanceId: string
@@ -10,14 +10,13 @@ type Props = {
 
 const AdditionalButtons = ({ instanceId }: Props) => {
   const { isMobile } = useDevice()
-
   const toInject = document.querySelector('.mobile-misc-tools-container')
 
   if (isMobile && toInject) {
     return (
       <>
-        {createPortal(
-          <>
+        {InjectBefore(
+          <Stack flexDirection={'column'}>
             <ViewModeToggle
               sx={{ flexDirection: 'column' }}
               toggleButtonProps={{
@@ -26,7 +25,7 @@ const AdditionalButtons = ({ instanceId }: Props) => {
                   border: 'none',
                   minWidth: '36px',
                   px: 0,
-                  py: 1,
+                  py: 1.3,
                   fontSize: '16px',
                   color: 'var(--icon-fill-color)',
                 },
@@ -38,48 +37,12 @@ const AdditionalButtons = ({ instanceId }: Props) => {
               id={instanceId}
               sx={{ minWidth: '36px', px: 0, fontSize: '16px' }}
             />
-          </>,
+          </Stack>,
           toInject,
         )}
       </>
     )
   }
-
-  // ) return (
-  //   <Box
-  //     sx={{
-  //       bgcolor: 'var(--island-bg-color)',
-  //       position: 'absolute',
-  //       top: 'calc(14rem - var(--editor-container-padding))',
-  //       right: 'calc(var(--editor-container-padding) * -1)',
-  //       display: 'flex',
-  //       flexDirection: 'column',
-  //       zIndex: '10',
-  //       borderTopLeftRadius: 'var(--border-radius-lg)',
-  //       borderBottomLeftRadius: 'var(--border-radius-lg)',
-  //     }}
-  //   >
-  //     <ViewModeToggle
-  //       sx={{ flexDirection: 'column' }}
-  //       toggleButtonProps={{
-  //         sx: {
-  //           border: 'none',
-  //           minWidth: '36px',
-  //           px: 0,
-  //           py: 1,
-  //           fontSize: '16px',
-  //           color: 'var(--icon-fill-color)',
-  //         },
-  //         size: 'small',
-  //       }}
-  //     />
-  //     <ShareButton
-  //       variant={'text'}
-  //       id={instanceId}
-  //       sx={{ minWidth: '36px', px: 0, fontSize: '16px' }}
-  //     />
-  //   </Box>
-  // )
 
   return (
     <Stack direction={'row'} spacing={1.5}>
